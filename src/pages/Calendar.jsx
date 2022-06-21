b4dd5701fae4572076a98247227dbc31b200f69c
Noden Sera <nodensera@gmail.com>
2022-06-21 00:53:37 -0300

    calendar schedule height

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -37,7 +37,7 @@ const Scheduler = () => {
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
       <ScheduleComponent
-        height="650px"
+        height="660px"
         ref={(schedule) => setScheduleObj(schedule)}
         selectedDate={new Date(2021, 0, 10)}
         eventSettings={{ dataSource: scheduleData }}
